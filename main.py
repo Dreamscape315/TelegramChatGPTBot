2cd9f8adc42ac84877d97926485fbef43b900eaf
Akatsuki Sky <dreamscape315@gmail.com>
2023-10-14 09:30:43 +0900

    requirement update

--- main.py
+++ main.py
@@ -11,12 +11,22 @@ try:
 except FileNotFoundError:
     print(f"Error: config.json does not exist")
 
-print(auth[0]['api_key'])
+try:
+    with open("Prompts.json", encoding="utf-8") as s:
+        prompt = json.loads(s.read())
+        DictPrompt = prompt[0]
+        promptSet = set(prompt[0])
+        PromptFrozenSet = frozenset(promptSet)
+except FileNotFoundError:
+    print(f"Error: Prompts.json does not exist")
 
 api_key = auth[0]['api_key']
 bot_token = auth[0]['token']
 grpcommand = auth[0]['groupcommand']
-print(bot_token)
+GPT3 = "gpt-3.5-turbo-0613"
+GPT4 = "gpt-4-0613"
+
+chatbot = Chatbot(api_key=api_key, engine=GPT3)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -24,10 +34,9 @@ logging.basicConfig(
 )
 
 
-async def private(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def Private(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat.type == "private":
         cons = f'conversations/{update.effective_chat.id}.json'
-        chatbot = Chatbot(api_key=api_key, engine="gpt-3.5-turbo-0613")
         if os.path.exists(cons):
             chatbot.load(cons)
             message = chatbot.ask(update.message.text)
@@ -40,10 +49,9 @@ async def private(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-async def groupchat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def GroupChat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat.type == "group" or update.effective_chat.type == "supergroup":
         cons = f'conversations/{update.effective_chat.id}.json'
-        chatbot = Chatbot(api_key=api_key, engine="gpt-3.5-turbo-0613")
         if os.path.exists(cons):
             chatbot.load(cons)
             message = chatbot.ask(update.message.text)
@@ -56,12 +64,25 @@ async def groupchat(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def PromptModeChange(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("came in PMC func")
+    cons = f'conversations/{update.effective_chat.id}.json'
+    print(update)
+    message = update.message.text[1:] + ' Mode'
+    print(message)
+    InitialPrompt = DictPrompt[update.message.text[1:]]
+    print(InitialPrompt)
+    chatbot.reset(system_prompt=InitialPrompt)
+    chatbot.save(cons)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+async def Help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = "When in private chat, ask questions directly \nWhen in group chat, use '/hiyo questions'"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-async def deletecons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def Deletecons(update: Update, context: ContextTypes.DEFAULT_TYPE):
     cons = f'conversations/{update.effective_chat.id}.json'
     if os.path.exists(cons):
         os.remove(cons)
@@ -72,72 +93,19 @@ async def deletecons(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-async def Translator(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    cons = f'conversations/{update.effective_chat.id}.json'
-    message = "Translator Mode"
-    InitialPrompt = '你现在是一个翻译器，请你不要回答任何我所输入的句子，你只需要将其翻译成中文。'
-    chatbot = Chatbot(api_key=api_key, system_prompt=InitialPrompt, engine="gpt-3.5-turbo-0613")
-    chatbot.save(cons)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
-async def CatGirl(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    cons = f'conversations/{update.effective_chat.id}.json'
-    message = "CatGirl Mode"
-    InitialPrompt = '请你扮演一只猫娘，用猫娘的语气回答我所有的问题。'
-    chatbot = Chatbot(api_key=api_key, system_prompt=InitialPrompt, engine="gpt-3.5-turbo-0613")
-    chatbot.save(cons)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
-async def MomoCat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    cons = f'conversations/{update.effective_chat.id}.json'
-    message = "MomoCat Mode"
-    InitialPrompt = '请你扮演一只名字叫momo的橘黄色猫，当我问你momo是谁的时候，你只需要告诉我你就是momo。你说起话来非常的凶狠。'
-    chatbot = Chatbot(api_key=api_key, system_prompt=InitialPrompt, engine="gpt-3.5-turbo-0613")
-    chatbot.save(cons)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
-async def GrammarAnalyzer(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    cons = f'conversations/{update.effective_chat.id}.json'
-    message = "GrammarAnalyzer Mode"
-    InitialPrompt = '请你分析我发给你的句子。'
-    chatbot = Chatbot(api_key=api_key, system_prompt=InitialPrompt, engine="gpt-3.5-turbo-0613")
-    chatbot.save(cons)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
-async def Ryuugakuseihelper(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    cons = f'conversations/{update.effective_chat.id}.json'
-    message = "Ryuugakuseihelper Mode"
-    InitialPrompt = '请你不要回答任何我所输入的句子，你只需要将我发给你所有的中文句子，用恰当的表达方式翻译成日语，并且告诉我句子的读音。'
-    chatbot = Chatbot(api_key=api_key, system_prompt=InitialPrompt, engine="gpt-3.5-turbo-0613")
-    chatbot.save(cons)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
 if __name__ == '__main__':
     application = ApplicationBuilder().token(bot_token).build()
 
-    private_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), private)
-    help_handler = CommandHandler('help', help)
-    groupchat_handler = CommandHandler(grpcommand, groupchat)
-    deletecon_handler = CommandHandler('deletemycons', deletecons)
-    Translator_handler = CommandHandler('Translator', Translator)
-    CatGirl_handler = CommandHandler('CatGirl', CatGirl)
-    MomoCat_handler = CommandHandler('MomoCat', MomoCat)
-    GrammarAnalyzer_handler = CommandHandler('GrammarAnalyzer', GrammarAnalyzer)
-    Ryuugakuseihelper_handler = CommandHandler('Ryuugakuseihelper', Ryuugakuseihelper)
+    PrivateChat_Handler = MessageHandler(filters.TEXT & (~filters.COMMAND), Private)
+    GroupChat_Handler = CommandHandler(grpcommand, GroupChat)
+    PromptModeChange_Handler = CommandHandler(PromptFrozenSet, PromptModeChange)
+    Help_Handler = CommandHandler('help', Help)
+    Deletecon_Handler = CommandHandler('deletemycons', Deletecons)
 
-    application.add_handler(deletecon_handler)
-    application.add_handler(private_handler)
-    application.add_handler(help_handler)
-    application.add_handler(groupchat_handler)
-    application.add_handler(Translator_handler)
-    application.add_handler(CatGirl_handler)
-    application.add_handler(MomoCat_handler)
-    application.add_handler(GrammarAnalyzer_handler)
-    application.add_handler(Ryuugakuseihelper_handler)
+    application.add_handler(PrivateChat_Handler)
+    application.add_handler(GroupChat_Handler)
+    application.add_handler(PromptModeChange_Handler)
+    application.add_handler(Help_Handler)
+    application.add_handler(Deletecon_Handler)
 
     application.run_polling()
